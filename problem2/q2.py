ebef68d881c4d494e1856da0986db963e31889e4
Harshit Varma <varmaharshit2@gmail.com>
2020-09-02 16:29:26 +0530

    Added Task-2 solution

--- problem2/q2.py
+++ problem2/q2.py
@@ -1,1 +1,19 @@
-# Enter your code here
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input",  "-in", type=str, required=True)
+parser.add_argument("--output", "-out", type=str, required=True)
+
+args = parser.parse_args()
+infile  = args.input
+outfile = args.output
+
+with open(infile, 'r') as inpf:
+    inpath  = inpf.readline()
+    outpath = os.path.realpath(inpath)
+    if (outpath[-2] == '/'): # Ends with a trailing backslash
+        outpath = outpath[:-2]
+    with open(outfile, 'w') as outf:
+        outf.write(outpath)
+
