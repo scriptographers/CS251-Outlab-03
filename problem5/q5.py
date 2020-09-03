234cd1a444fad9323b6e6f4ecd4244b44c0b6779
devansh-dvj <devansh.dvj@gmail.com>
2020-09-04 00:50:56 +0530

    Task 5: Setup input and output as defined

--- problem5/q5.py
+++ problem5/q5.py
@@ -5,3 +5,16 @@ def rotate(m):
     for i in range(0, len(m), 1):
         temp.append([m[j][i] for j in range(len(m) - 1, -1, -1)])
     return temp
+
+
+def print_mat(m):
+    for row in m:
+        print(' '.join(row))
+
+
+if __name__ == '__main__':
+    sq_mat = []
+    sq_mat.append(input().split())
+    for i in range(1, len(sq_mat[0])):
+        sq_mat.append(input().split())
+    print_mat(rotate(sq_mat))
