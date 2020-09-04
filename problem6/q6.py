c3278cfd28fb099c67858b7267287bd9e0cf110d
devansh-dvj <devansh.dvj@gmail.com>
2020-09-04 22:41:45 +0530

    Task 6 - Updated without list comprehension

--- problem6/q6.py
+++ problem6/q6.py
@@ -1,5 +1,17 @@
 # Enter your code here
 
+#################### Using List comprehension but using def ####################
+# def collapse(L):
+#     if isinstance(L, list) and L:
+#         return ' '.join([collapse(elem) for elem in L])
+#     else:
+#         return L
+################################################################################
+
+################### Using List comprehension and using lambda ##################
+# collapse = lambda l: " ".join([collapse(elem) for elem in l]) if (isinstance(l, list) and l) else '' if l == [] else l
+################################################################################
+
 ################ Without using List comprehension but using def ################
 # def collapse(l):
 #     if isinstance(l, list) and l:
@@ -11,22 +23,12 @@
 ################################################################################
 
 ############## Without using List comprehension and using lambda ###############
-# collapse = lambda l: " ".join((collapse(l[:-1]) + ' ' + collapse(l.pop())).split()) if (isinstance(l, list) and l) else '' if l == [] else l
+# collapse = lambda l: (collapse(l[:-1]) + " " + collapse(l.pop())) if (isinstance(l, list) and l) else '' if l == [] else l
+collapse = lambda l: ' '.join((collapse(l[:-1]) + ' ' + collapse(l.pop())).split()) if (isinstance(l, list) and l) else '' if l == [] else l
 ################################################################################
-
-#################### Using List comprehension but using def ####################
-# def collapse(L):
-#     if isinstance(L, list) and L:
-#         return ' '.join([collapse(elem) for elem in L])
-#     else:
-#         return L
-################################################################################
-
-################### Using List comprehension and using lambda ##################
-collapse = lambda l: " ".join([collapse(elem) for elem in l]) if (isinstance(l, list) and l) else '' if l == [] else l
-################################################################################
-
 
 # Tests
-# x = [["ab"], ["ac", ["ad", ["ae"]]], ["af"]]
-# print(collapse(x))
+x = [["ab"], ["ac", ["ad", ["ae", "aas"]]], ["af"]]
+y = [[[[[['']]]]]]
+print(collapse(x))
+print(collapse(y))
