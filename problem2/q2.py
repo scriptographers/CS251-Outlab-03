1d8df817cc465eaa9b100fc8e437fca1fb382b44
devansh-dvj <devansh.dvj@gmail.com>
2020-09-06 06:13:03 +0000

    Task 2: Final and tested on docker

--- problem2/q2.py
+++ problem2/q2.py
@@ -2,15 +2,15 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input",  "-inp", type=str, required=True)
+parser.add_argument("--input", "-inp", type=str, required=True)
 parser.add_argument("--output", "-out", type=str, required=True)
 
 args = parser.parse_args()
-infile  = args.input
+infile = args.input
 outfile = args.output
 
 with open(infile, 'r') as inpf:
-    inpath  = inpf.readline()
+    inpath = inpf.readline()
     outpath = os.path.realpath(inpath)
     with open(outfile, 'w') as outf:
         outf.write(outpath)
